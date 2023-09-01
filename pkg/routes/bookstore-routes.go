83d926ea56d4a5835694761994b24c0aa99e571e
uduak <lorduduak@gmail.com>
2023-09-01 09:52:13 +0100

    added controllers

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,8 +1,9 @@
 package routes
 
 import (
-	"github.com/Ueetim/go-bookstore/pkg/controllers"
 	"github.com/gorilla/mux"
+	
+	"github.com/Ueetim/go-bookstore/pkg/controllers"
 )
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
